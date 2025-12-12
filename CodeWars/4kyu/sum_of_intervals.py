d91ee74fe61b1467e0d55a70e2c61805275b40f0
Nemuidere <olokoxthelegend@gmail.com>
2025-12-12 19:57:53 +0100

    chore: fixed some errors

--- CodeWars/4kyu/sum_of_intervals.py
+++ CodeWars/4kyu/sum_of_intervals.py
@@ -3,7 +3,7 @@ https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/python
 
 Write a function called sumIntervals/sum_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
 
-Intervals
+Interval
 Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
 
 Overlapping Intervals
